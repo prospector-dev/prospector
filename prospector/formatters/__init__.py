b687f0989f67054eefdaa42c04708f5122f89e31
Jason Simeone <jay@classless.net>
2013-12-31 09:12:44 -0500

    Added a 'grouped' output formatter.

--- prospector/formatters/__init__.py
+++ prospector/formatters/__init__.py
@@ -1,6 +1,7 @@
-from prospector.formatters import json, text
+from prospector.formatters import json, text, grouped
 
 FORMATTERS = {
     'json': json.format_messages,
-    'text': text.format_messages
+    'text': text.format_messages,
+    'grouped': grouped.format_messages,
 }
