e40de1954e66f1f6b177ac80b869edea72fecbb5
carlio <git@carlcrowder.com>
2014-08-03 11:16:13 +0200

    Fixing a mild style-moan

--- prospector/config.py
+++ prospector/config.py
@@ -63,19 +63,19 @@ def build_manager():
 
 
 def build_default_sources():
-    sources = []
-
-    sources.append(build_command_line_source())
-    sources.append(soc.EnvironmentVariableSource())
-    sources.append(soc.ConfigFileSource((
-        '.prospectorrc',
-        'setup.cfg',
-        'tox.ini',
-    )))
-    sources.append(soc.ConfigFileSource((
-        soc.ConfigDirectory('.prospectorrc'),
-        soc.HomeDirectory('.prospectorrc'),
-    )))
+    sources = [
+        build_command_line_source(),
+        soc.EnvironmentVariableSource(),
+        soc.ConfigFileSource((
+            '.prospectorrc',
+            'setup.cfg',
+            'tox.ini',
+        )),
+        soc.ConfigFileSource((
+            soc.ConfigDirectory('.prospectorrc'),
+            soc.HomeDirectory('.prospectorrc'),
+        ))
+    ]
 
     return sources
 
