6d6c38bba6a9831d1695f2faf400a9abc80c2d24
carlio <git@carlcrowder.com>
2014-09-26 19:36:17 +0200

    Another attempt at properly ascertaining the sys.path

--- prospector/__pkginfo__.py
+++ prospector/__pkginfo__.py
@@ -1,5 +1,5 @@
 
-VERSION = (0, 6, 1)
+VERSION = (0, 6, 2)
 
 def get_version():
     return '.'.join([str(v) for v in VERSION])
--- prospector/finder.py
+++ prospector/finder.py
@@ -37,6 +37,45 @@ class FoundFiles(object):
         for module in self.modules:
             yield(os.path.abspath(os.path.join(self.rootpath, module)))
 
+    def get_minimal_syspath(self):
+        """
+        Provide a list of directories that, when added to sys.path, would enable
+        any of the discovered python modules to be found
+        """
+        # for each package, we want the first directory which is a package
+        # ie, a/b/c/__init.py will be also found by adding a/ to the syspath if a and a/b are also packages
+        syspath = set()
+        # sorting into order will mean that 'higher' packages are in the set by the time we look at 'lower' packages
+        packages = sorted(self.packages, key=lambda x: len(x))
+        for package in packages:
+            dirs = package.split(os.path.sep)
+            if len(dirs) <= 1:
+                syspath.add(package)
+                continue
+            for i in range(1, len(dirs)):
+                testpath = os.path.join(*dirs[:i])
+                if testpath in syspath:
+                    break
+            else:
+                syspath.add(package)
+
+        # we also need to add directories containing any of the python modules discovered that are not
+        # in a package
+        for module in self.modules:
+            module_dir = os.path.dirname(module)
+            dirs = module_dir.split(os.path.sep)
+            if len(dirs) <= 1:
+                syspath.add(module_dir)
+                continue
+            for i in range(1, len(dirs)):
+                testpath = os.path.join(*dirs[:i])
+                if testpath in syspath:
+                    break
+            else:
+                syspath.add(module_dir)
+
+        return [os.path.join(self.rootpath, p) for p in syspath]
+
 
 def _find_paths(ignore, curpath, rootpath):
     files, modules, packages, directories = [], [], [], []
