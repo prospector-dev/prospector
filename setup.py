813f7f3c0f05889c90f3f49c990a88639972a0b9
carlio <git@carlcrowder.com>
2014-09-27 21:20:06 +0200

    Increasing required version of some dependencies

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ _PACKAGES = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
 _INSTALL_REQUIRES = [
     'pylint>=1.3',
     'pylint-celery>=0.3',
-    'pylint-django>=0.5',
-    'pylint-plugin-utils>=0.2.1',
+    'pylint-django>=0.5.4',
+    'pylint-plugin-utils>=0.2.2',
     'pylint-common>=0.2.1',
-    'requirements-detector>=0.2.2',
+    'requirements-detector>=0.3',
     'setoptconf>=0.2.0',
     'dodgy>=0.1.5',
     'pyyaml',
