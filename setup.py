78369b0c64ffcdea71b36770eacebdd67b6053c5
carlio <git@carlcrowder.com>
2014-11-30 12:59:49 +0100

    Restricting pylint version to 1.3 to ensure that prospector still can handle Python 2.6 in the 0.7 releases. Python 2.6 support will be removed (and therefore pylint 1.4+ will be added) in the 0.8 releases

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from prospector import __pkginfo__
 _PACKAGES = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
 
 _INSTALL_REQUIRES = [
-    'pylint>=1.3',
+    'pylint>=1.3,<1.4',
     'pylint-celery>=0.3',
     'pylint-django>=0.5.5',
     'pylint-plugin-utils>=0.2.2',
