5aa6d92a26ce30a3b47c61d1f95c29589864c42a
carlio <git@carlcrowder.com>
2014-12-26 09:17:57 +0000

    Turning off the pylint safety mechanism around loading C-extensions, since it causes inference to fail badly on almost all libraries with C-extensions

--- prospector/tools/pylint/__init__.py
+++ prospector/tools/pylint/__init__.py
@@ -82,6 +82,15 @@ class PylintTool(ToolBase):
 
         self._args = linter.load_command_line_configuration(check_paths)
 
+        # Pylint 1.4 introduced the idea of explicitly specifying which C-extensions
+        # to load. This is because doing so allows them to execute any code whatsoever,
+        # which is considered to be unsafe. The following option turns off this, allowing
+        # any extension to load again, since any setup.py can execute arbitrary code and
+        # the safety gained through this approach seems minimal. Leaving it on means
+        # that the inference engine cannot do much inference on modules with C-extensions
+        # which is a bit useless.
+        linter.set_option('unsafe-load-any-extension')
+
         # The warnings about disabling warnings are useful for figuring out
         # with other tools to suppress messages from. For example, an unused
         # import which is disabled with 'pylint:disable=W0611' will still
