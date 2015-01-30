43583681ce7c60bd8b6242397a817ef269cc957b
carlio <git@carlcrowder.com>
2015-01-30 08:31:27 +0100

    Renaming pyflake error codes to match those of flake8 (see http://flake8.readthedocs.org/en/latest/warnings.html)

--- prospector/tools/pyflakes/__init__.py
+++ prospector/tools/pyflakes/__init__.py
@@ -12,20 +12,39 @@ __all__ = (
 )
 
 
+# Prospector uses the same pyflakes codes as flake8 defines,
+# see http://flake8.readthedocs.org/en/latest/warnings.html
+# and https://bitbucket.org/tarek/flake8/src/a209fb69350c572c9b2d7b4b09c7657be153be5e/flake8/_pyflakes.py
 _MESSAGE_CODES = {
-    'UnusedImport': 'FL0001',
-    'RedefinedWhileUnused': 'FL0002',
-    'RedefinedInListComp': 'FL0003',
-    'ImportShadowedByLoopVar': 'FL0004',
-    'ImportStarUsed': 'FL0005',
-    'UndefinedName': 'FL0006',
-    'DoctestSyntaxError': 'FL0007',
-    'UndefinedExport': 'FL0008',
-    'UndefinedLocal': 'FL0009',
-    'DuplicateArgument': 'FL0010',
-    'Redefined': 'FL0011',
-    'LateFutureImport': 'FL0012',
-    'UnusedVariable': 'FL0013',
+    'UnusedImport': 'F401',
+    'ImportShadowedByLoopVar': 'F402',
+    'ImportStarUsed': 'F403',
+    'LateFutureImport': 'F404',
+    'Redefined': 'F810',
+    'RedefinedWhileUnused': 'F811',
+    'RedefinedInListComp': 'F812',
+    'UndefinedName': 'F821',
+    'UndefinedExport': 'F822',
+    'UndefinedLocal': 'F823',
+    'DuplicateArgument': 'F831',
+    'UnusedVariable': 'F841',
+}
+
+# The old prospector codes were deprecated in favour of using the codes
+# defined by flake8. This maps the old codes to the new codes.
+LEGACY_CODE_MAP = {
+    'FL0001': 'F401',
+    'FL0002': 'F811',
+    'FL0003': 'F812',
+    'FL0004': 'F402',
+    'FL0005': 'F403',
+    'FL0006': 'F821',
+    'FL0008': 'F822',
+    'FL0009': 'F823',
+    'FL0010': 'F831',
+    'FL0011': 'F810',
+    'FL0012': 'F404',
+    'FL0013': 'F841',
 }
 
 
@@ -44,7 +63,7 @@ class ProspectorReporter(Reporter):
             code=None,
             message=None):
 
-        code = code or 'FL0000'
+        code = code or 'F999'
         if code in self.ignore:
             return
 
@@ -66,7 +85,7 @@ class ProspectorReporter(Reporter):
     def unexpectedError(self, filename, msg):  # noqa
         self.record_message(
             filename=filename,
-            code='FL9997',
+            code='F999',
             message=msg,
         )
 
@@ -76,12 +95,12 @@ class ProspectorReporter(Reporter):
             filename=filename,
             line=lineno,
             character=offset,
-            code='FL9998',
+            code='F999',
             message=msg,
         )
 
     def flake(self, message):
-        code = _MESSAGE_CODES.get(message.__class__.__name__, 'FL9999')
+        code = _MESSAGE_CODES.get(message.__class__.__name__, 'F999')
 
         self.record_message(
             filename=message.filename,
