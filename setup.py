0649166c5553782fdcd89008c8ee536906d94ffd
carlio <git@carlcrowder.com>
2015-11-21 10:33:06 +0700

    Adding python3.5 to classifiers

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ _CLASSIFIERS = (
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
     'License :: OSI Approved :: '
     'GNU General Public License v2 or later (GPLv2+)',
 )
