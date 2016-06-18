30a04b9f6976aabc094725ee12aecbd739fb04a7
Dmitriy Volkov <wldhx+vcs+github_com@wldhx.me>
2016-06-18 11:15:18 +0300

    Bump dependency on pyroma to `>=2.0.2` (#168)

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ _CLASSIFIERS = (
 _OPTIONAL = {
     'with_frosted': ('frosted>=1.4.1',),
     'with_vulture': ('vulture>=0.6',),
-    'with_pyroma': ('pyroma>=1.6,<2.0',),
+    'with_pyroma': ('pyroma>=2.0.2',),
     'with_pep257': (),  # note: this is no longer optional, so this option will be removed in a future release
 }
 with_everything = [req for req_list in _OPTIONAL.values() for req in req_list]
