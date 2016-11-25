d83f4dae4ecba158ac3104cdd17851a2094e010f
Holger Joukl <holger.joukl@LBBW.de>
2016-11-25 15:06:44 +0100

    Revamped pep8/pycodestyle test to expose correct module-based expectations.
    Now set up to fail for the current incorrect behaviour of silently ignoring
    modules in non-package directories (#179).

--- tests/tools/test_pep8.py
+++ tests/tools/test_pep8.py
@@ -1,22 +1,34 @@
 import os
 import tempfile
+import re
 from unittest import TestCase
 from prospector.finder import find_python
 from prospector.tools.pep8 import Pep8Tool
+from prospector.config import ProspectorConfig
 
 
-# A very basic stub to avoid creating a real ProspectorConfig, which wants
-# command line (sys.argv) args and whatnot.
-# Needed for handing to the Pep8Tool.configure(...) method, should expose the
-# ProspectorConfig interface accessed in this method.
-class StubProspectorConfig:
-    # not actually used for anything here, default to s.th. "big"
-    _max_line_length = 300
+# A very basic stub to avoid creating a real ProspectorConfig, which isn't
+# esasily tweakable for tests "from the outside".
+# Needed for handing into the Pep8Tool.configure(...) method, should expose at
+# least the ProspectorConfig interface accessed by this method.
+class StubProspectorConfig(object):
 
-    def __init__(self, external_config=None):
+    def __init__(self, external_config=None, ignores=None,
+                 max_line_length=300):
         # external_config: optional file path to pep8/pycodestyle config file
+        # ignores: list of regular expressions strings of (relative) paths to
+        #     ignore
+        # max_line_length: value for the max_line_length property, not
+        #     currently actually used for anything here, default to s.th.
+        #     "big"
         self.external_config = external_config
+        if ignores is None:
+            self.ignores = [] 
+        else:
+            self.ignores = [re.compile(patt) for patt in ignores]
+        self._max_line_length = max_line_length
 
+    # stubbed ProspectorConfig API
     @property
     def max_line_length(self):
         return self._max_line_length
@@ -34,10 +46,27 @@ class StubProspectorConfig:
         return {}
 
 
+def dummy_checker():
+    # create a fake checker that gathers the filenames into
+    # DummyChecker.filenames class attribute
+    class DummyChecker(object):
+        filenames = []
+
+        def __init__(self, filename, *args, **kwargs):
+            self.filenames.append(filename)
+
+        def check_all(self, *args, **kwargs):
+            pass
+
+    return DummyChecker
+
+
 # helper function
-def rooted(root, paths):
+def prepend_root(paths, root=None):
     """Return list of paths with root dir prepended to each path."""
-    return [os.path.normpath(os.path.join(root, p)) for p in paths]
+    if root:
+        return [os.path.normpath(os.path.join(root, p)) for p in paths]
+    return paths
 
 
 class TestProspectorStyleGuidePathConfig(TestCase):
@@ -45,14 +74,16 @@ class TestProspectorStyleGuidePathConfig(TestCase):
     # paths to check.
     # We need to create the ProspectorStyleGuide through a Pep8Tool instance
     # and use finder.find_python to create the input for Pep8Tool.configure()
-    def _setup_styleguide(self, paths, external_config=None):
+
+    def _setup_styleguide(self, paths, ignores=None, external_config=None):
         # A helper to create the StyleGuide object.
         # root: the root directory for the relative paths given in the paths
-        # and expected parameters
+        #     and expected parameters
         pep8tool = Pep8Tool()
-        config = StubProspectorConfig(external_config=external_config)
+        config = StubProspectorConfig(
+            external_config=external_config, ignores=ignores)
         found_python = find_python(
-            ignores=[], paths=paths,
+            ignores=config.ignores, paths=paths,
             explicit_file_mode=True if len(paths)>1 else False)
 
         configured_by, _ = pep8tool.configure(config, found_python)
@@ -61,6 +92,9 @@ class TestProspectorStyleGuidePathConfig(TestCase):
                 "Configuration found at %s" % external_config)
             self.assertEqual(configured_by, expected_configuration_message) 
         prospector_styleguide = pep8tool.checker
+        # inject file paths-gathering dummy checker and invoke it
+        prospector_styleguide.checker_class = dummy_checker()
+        prospector_styleguide.check_files()
         return prospector_styleguide
 
     def test_paths_dir_arg(self):
@@ -69,29 +103,53 @@ class TestProspectorStyleGuidePathConfig(TestCase):
             os.path.dirname(__file__), '..', 'finder', 'testdata',
             'dirs_mods_packages')
         # use the root directory itself as find_python input arg:
-        paths = rooted(root, [''])
+        paths = prepend_root([''], root)
         # all Python modules in the directory tree are expected (but nothing 
         # else)
-        expected = rooted(
-            root,
+        expected = prepend_root(
             ['package/__init__.py',
              'package/subpackage/__init__.py',
-             'package/subpackage/subpackage_module.py',
-             # 'package/subpackage/subpackage_plain.txt',
-             'package/package_module.py',
-             # 'package/package_plain.txt',
-             'nonpackage/nonpackage_module.py',
-             # 'nonpackage/nonpackage_plain.txt',
-             'module.py',
-             # 'plain.txt',
-             ])
-        expected = rooted(
-            root,
-            ['package',
-             'package/subpackage',
-             ])
+             'package/subpackage/subpackage_module1.py',
+             'package/subpackage/subpackage_module2.py',
+             'package/package_module1.py',
+             'package/package_module2.py',
+             'nonpackage/nonpackage_module1.py',
+             'nonpackage/nonpackage_module2.py',
+             'module1.py',
+             'module2.py',
+             ], root)
+
         prospector_styleguide = self._setup_styleguide(paths)
-        self.assertEqual(sorted(prospector_styleguide.paths), sorted(expected))
+        self.assertEqual(
+            sorted(prospector_styleguide.checker_class.filenames),
+            sorted(expected))
+
+    def test_paths_dir_arg_prospector_ignore_patterns_cfg(self):
+        # test corresponding to a single command line PATH dir arg, with
+        # with prospectorer ignore-patterns 
+        root = os.path.join(
+            os.path.dirname(__file__), '..', 'finder', 'testdata',
+            'dirs_mods_packages')
+        # use the root directory itself as find_python input arg:
+        paths = prepend_root([''], root)
+
+        ignores = ['(^|/)subpackage(/|$)', 'nonpackage', '^package/module1.py$']
+        expected = prepend_root(
+            ['package/__init__.py',
+             #'package/subpackage/__init__.py',
+             #'package/subpackage/subpackage_module1.py',
+             #'package/subpackage/subpackage_module2.py',
+             #'package/package_module1.py',
+             'package/package_module2.py',
+             #'nonpackage/nonpackage_module1.py',
+             #'nonpackage/nonpackage_module2.py',
+             'module1.py',
+             'module2.py',
+             ], root)
+        prospector_styleguide = self._setup_styleguide(paths, ignores=ignores)
+        self.assertEqual(
+            sorted(prospector_styleguide.checker_class.filenames),
+            sorted(expected))
 
     def test_paths_dir_arg_pep8_exclude_cfg(self):
         # test corresponding to a single command line PATH dir arg, with
@@ -100,58 +158,70 @@ class TestProspectorStyleGuidePathConfig(TestCase):
             os.path.dirname(__file__), '..', 'finder', 'testdata',
             'dirs_mods_packages')
         # use the root directory itself as find_python input arg:
-        paths = rooted(root, [''])
+        paths = prepend_root([''], root)
 
         # set pep8/pycodestyle exclude config option
         external_config_contents = """
 [pep8]
 exclude = subpackage
         """
-        expected = rooted(
-            root,
-            ['package',
-             ])
-        with tempfile.NamedTemporaryFile(delete=False) as f:
+        expected = prepend_root(
+            ['package/__init__.py',
+             #'package/subpackage/__init__.py',
+             #'package/subpackage/subpackage_module1.py',
+             #'package/subpackage/subpackage_module2.py',
+             'package/package_module1.py',
+             'package/package_module2.py',
+             'nonpackage/nonpackage_module1.py',
+             'nonpackage/nonpackage_module2.py',
+             'module1.py',
+             'module2.py',
+             ], root)
+        with tempfile.NamedTemporaryFile() as f:
             f.write(external_config_contents)
             f.flush()
             prospector_styleguide = self._setup_styleguide(
                 paths, external_config=f.name)
-            # exclusion of paths happens when pydodestyle checking is actually
-            # run, simulate this by using the .excluded() method
-            paths_after_exclusion = [
-                p for p in prospector_styleguide.paths 
-                if not prospector_styleguide.excluded(p)]
             self.assertEqual(
-                sorted(paths_after_exclusion), sorted(expected))
+                sorted(prospector_styleguide.checker_class.filenames),
+                sorted(expected))
 
     def test_paths_py_modules_arg(self):
         # test corresponding to multiple Python module files PATHs args
         root = os.path.join(
             os.path.dirname(__file__), '..', 'finder', 'testdata',
             'dirs_mods_packages')
-        paths = rooted(
-            root,
+        paths = prepend_root(
             ['package/__init__.py',
              'package/subpackage/__init__.py',
-             'package/subpackage/subpackage_module.py',
-             'package/package_module.py',
-             'nonpackage/nonpackage_module.py',
-             'module.py',
-             ])
+             'package/subpackage/subpackage_module1.py',
+             'package/subpackage/subpackage_module2.py',
+             'package/package_module1.py',
+             'package/package_module2.py',
+             'nonpackage/nonpackage_module1.py',
+             'nonpackage/nonpackage_module2.py',
+             'module1.py',
+             'module2.py',
+             ], root)
 
         # all Python modules given as paths args are expected (but nothing 
         # else)
-        expected = rooted(
-            root,
+        expected = prepend_root(
             ['package/__init__.py',
              'package/subpackage/__init__.py',
-             'package/subpackage/subpackage_module.py',
-             'package/package_module.py',
-             'nonpackage/nonpackage_module.py',
-             'module.py',
-             ])
+             'package/subpackage/subpackage_module1.py',
+             'package/subpackage/subpackage_module2.py',
+             'package/package_module1.py',
+             'package/package_module2.py',
+             'nonpackage/nonpackage_module1.py',
+             'nonpackage/nonpackage_module2.py',
+             'module1.py',
+             'module2.py',
+             ], root)
         prospector_styleguide = self._setup_styleguide(paths)
-        self.assertEqual(sorted(prospector_styleguide.paths), sorted(expected))
+        self.assertEqual(
+            sorted(prospector_styleguide.checker_class.filenames),
+            sorted(expected))
 
     def test_paths_files_arg(self):
         # test corresponding to multiple files PATHs args (Python modules and
@@ -159,37 +229,46 @@ exclude = subpackage
         root = os.path.join(
             os.path.dirname(__file__), '..', 'finder', 'testdata',
             'dirs_mods_packages')
-        paths = rooted(
-            root,
+        paths = prepend_root(
             ['package/__init__.py',
              'package/subpackage/__init__.py',
-             'package/subpackage/subpackage_module.py',
              'package/subpackage/subpackage_plain.txt',
-             'package/package_module.py',
+             'package/subpackage/subpackage_module1.py',
+             'package/subpackage/subpackage_module2.py',
+             'package/package_module1.py',
+             'package/package_module2.py',
              'package/package_plain.txt',
-             'nonpackage/nonpackage_module.py',
+             'nonpackage/nonpackage_module1.py',
+             'nonpackage/nonpackage_module2.py',
              'nonpackage/nonpackage_plain.txt',
-             'module.py',
+             'module1.py',
+             'module2.py',
              'plain.txt',
-             ])
+             ], root)
 
         # all files given as path args are expected (but nothing 
         # else)
         # TODO: Is this how it should work? Should the tools cater for
         # TODO: filtering non-Python files?
-        expected = rooted(
-            root,
+        expected = prepend_root(
             ['package/__init__.py',
              'package/subpackage/__init__.py',
-             'package/subpackage/subpackage_module.py',
              'package/subpackage/subpackage_plain.txt',
-             'package/package_module.py',
+             'package/subpackage/subpackage_module1.py',
+             'package/subpackage/subpackage_module2.py',
+             'package/package_module1.py',
+             'package/package_module2.py',
              'package/package_plain.txt',
-             'nonpackage/nonpackage_module.py',
+             'nonpackage/nonpackage_module1.py',
+             'nonpackage/nonpackage_module2.py',
              'nonpackage/nonpackage_plain.txt',
-             'module.py',
+             'module1.py',
+             'module2.py',
              'plain.txt',
-             ])
+             ], root)
+
         prospector_styleguide = self._setup_styleguide(paths)
-        self.assertEqual(sorted(prospector_styleguide.paths), sorted(expected))
+        self.assertEqual(
+            sorted(prospector_styleguide.checker_class.filenames),
+            sorted(expected))
 
