74fb45b53e8ab09ba3a970d36f4ade2da0053047
Carlos Coelho <carlos@vinta.com.br>
2017-08-12 17:54:14 -0300

    Format mypy messages into proper prospector messages

--- prospector/tools/mypy/__init__.py
+++ prospector/tools/mypy/__init__.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 
 from mypy import api
 
+from prospector.message import Location, Message
 from prospector.tools import ToolBase
 
 
@@ -16,7 +17,30 @@ class MypyTool(ToolBase):
     def __init__(self, *args, **kwargs):
         super(MypyTool, self).__init__(*args, **kwargs)
         self.checker = api
+        self.options = ['--show-column-numbers']
 
     def run(self, found_files):
-        import pdb ; pdb.set_trace()
-        mypy = self.checker.run(found_files)
+        paths = [path for path in found_files.iter_module_paths()]
+        paths.extend(self.options)
+        report, err, status = self.checker.run(paths)
+        messages = []
+
+        for message in report.splitlines():
+            path, line, char, err_type, *err_msg = message.split(':')
+            location = Location(
+                path=path,
+                module=None,
+                function=None,
+                line=line,
+                character=char,
+                absolute_path=True
+            )
+            message = Message(
+                source='mypy',
+                code=err_type,
+                location=location,
+                message=''.join(err_msg).strip()
+            )
+            messages.append(message)
+
+        return messages
