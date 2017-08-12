587ecb7959db491792e3f536c9fd56555fb71c2b
Carlos Coelho <carlos@vinta.com.br>
2017-08-12 18:56:56 -0300

    Added support for mypy cmd options

--- prospector/tools/mypy/__init__.py
+++ prospector/tools/mypy/__init__.py
@@ -19,6 +19,54 @@ class MypyTool(ToolBase):
         self.checker = api
         self.options = ['--show-column-numbers']
 
+    def configure(self, prospector_config, _):
+        options = prospector_config.tool_options('mypy')
+
+        follow_imports = options.get('follow-imports', 'normal')
+        ignore_missing_imports = options.get('ignore-missing-imports', False)
+        implict_optional = options.get('implict-optional', False)
+        python_2_mode = options.get('python-2-mode', False)
+        strict_optional = options.get('strict-optional', False)
+
+        allowed_options = options.get('allow', [])
+        check_options = options.get('check', [])
+        disallowed_options = options.get('disallow', [])
+        no_check_options = options.get('no-check', [])
+        no_warn_options = options.get('no-warn', [])
+        warn_options = options.get('warn', [])
+
+        self.options.append('--follow-imports=%s' % follow_imports)
+
+        if ignore_missing_imports:
+            self.options.append('--ignore-missing-imports')
+
+        if implict_optional:
+            self.options.append('--implict-optional')
+
+        if python_2_mode:
+            self.options.append('--py2')
+
+        if strict_optional:
+            self.options.append('--strict-optional')
+
+        for entry in allowed_options:
+            self.options.append('--allow-%s' % entry)
+        
+        for entry in check_options:
+            self.options.append('--check-%s' % entry)
+
+        for entry in disallowed_options:
+            self.options.append('--disallow-%s' % entry)
+
+        for entry in no_check_options:
+            self.options.append('--no-check-%s' % entry)
+
+        for entry in no_warn_options:
+            self.options.append('--no-warn-%s' % entry)
+
+        for entry in warn_options:
+            self.options.append('--warn-%s' % entry)
+
     def run(self, found_files):
         paths = [path for path in found_files.iter_module_paths()]
         paths.extend(self.options)
