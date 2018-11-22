4eebe2e84f4752edee82992ef9d5b2685a88100d
carlio <mail@carlcrowder.com>
2018-11-22 15:14:21 +0100

    [fixes #296] Splitting output format from output file(s) using generic separator rather than OS specific path separator

--- prospector/config/datatype.py
+++ prospector/config/datatype.py
@@ -1,11 +1,11 @@
-from os import pathsep
+import re
 
 from setoptconf.datatype import Choice
 
 
 class OutputChoice(Choice):
     def sanitize(self, value):
-        splitted_value = value.split(pathsep)
-        output_format, output_targets = splitted_value[0], splitted_value[1:]
+        parsed = re.split(r'[;:]', value)
+        output_format, output_targets = parsed[0], parsed[1:]
         validated_format =  super(OutputChoice, self).sanitize(output_format)
         return (output_format, output_targets)
