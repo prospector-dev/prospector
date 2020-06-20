fdfe4afe516077203c55f3462bc9a62736d38521
Carlos Coelho <carlos@vinta.com.br>
2020-06-20 15:54:06 -0300

    Add test to make sure false positive is not raised by pylint

--- tests/tools/pylint/test_pylint_tool.py
+++ tests/tools/pylint/test_pylint_tool.py
@@ -9,16 +9,27 @@ from prospector.tools.pylint import PylintTool
 
 
 class TestPylintTool(TestCase):
-    def setUp(self):
-        with patch("sys.argv", [""]):
-            self.config = ProspectorConfig()
-        self.pylint_tool = PylintTool()
-
     def test_absolute_path_is_computed_correctly(self):
+        pylint_tool, config = self._get_pylint_tool_and_prospector_config()
         root = os.path.join(os.path.dirname(__file__), "testpath", "test.py")
         root_sep_split = root.split(os.path.sep)
         root_os_split = os.path.split(root)
         found_files = find_python([], [root], explicit_file_mode=True)
-        self.pylint_tool.configure(self.config, found_files)
-        self.assertNotEqual(self.pylint_tool._args, [os.path.join(*root_sep_split)])
-        self.assertEqual(self.pylint_tool._args, [os.path.join(*root_os_split)])
+        pylint_tool.configure(config, found_files)
+        self.assertNotEqual(pylint_tool._args, [os.path.join(*root_sep_split)])
+        self.assertEqual(pylint_tool._args, [os.path.join(*root_os_split)])
+
+    def test_wont_throw_false_positive_relative_beyond_top_level(self):
+        with patch("os.getcwd", return_value=os.path.realpath("tests/tools/pylint/testpath/")):
+            pylint_tool, config = self._get_pylint_tool_and_prospector_config()
+        root = os.path.join(os.path.dirname(__file__), "testpath", "src", "mcve", "foobar.py")
+        found_files = find_python([], [root], explicit_file_mode=True)
+        pylint_tool.configure(config, found_files)
+        messages = pylint_tool.run(found_files)
+        self.assertListEqual(messages, [])
+
+    def _get_pylint_tool_and_prospector_config(self):
+        with patch("sys.argv", [""]):
+            config = ProspectorConfig()
+        pylint_tool = PylintTool()
+        return pylint_tool, config
---
+++ tests/tools/pylint/testpath/src/mcve/foobar.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from .foo import print_foo
+
+
+def print_foobar():
+    print_foo()
+    print("bar")
