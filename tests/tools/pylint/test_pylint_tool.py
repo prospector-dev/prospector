203903562f40abfa51fda3b52fd89d8f7c3b57c9
Carlos Coelho <carlos@vinta.com.br>
2020-06-23 19:31:26 -0300

    Check for useless suppression not being thrown

--- tests/tools/pylint/test_pylint_tool.py
+++ tests/tools/pylint/test_pylint_tool.py
@@ -9,6 +9,12 @@ from prospector.tools.pylint import PylintTool
 
 
 class TestPylintTool(TestCase):
+    def _get_pylint_tool_and_prospector_config(self, argv_patch=[""]):
+        with patch("sys.argv", argv_patch):
+            config = ProspectorConfig()
+        pylint_tool = PylintTool()
+        return pylint_tool, config
+
     def test_absolute_path_is_computed_correctly(self):
         pylint_tool, config = self._get_pylint_tool_and_prospector_config()
         root = os.path.join(os.path.dirname(__file__), "testpath", "test.py")
@@ -28,8 +34,13 @@ class TestPylintTool(TestCase):
         messages = pylint_tool.run(found_files)
         self.assertListEqual(messages, [])
 
-    def _get_pylint_tool_and_prospector_config(self):
-        with patch("sys.argv", [""]):
-            config = ProspectorConfig()
-        pylint_tool = PylintTool()
-        return pylint_tool, config
+    def test_wont_throw_useless_suppression(self):
+        with patch("os.getcwd", return_value=os.path.realpath("tests/tools/pylint/testpath/")):
+            pylint_tool, config = self._get_pylint_tool_and_prospector_config(argv_patch=["", "-t", "pylint"])
+        root = os.path.join(os.path.dirname(__file__), "testpath", "test_useless_suppression.py")
+        found_files = find_python([], [root], explicit_file_mode=True)
+        pylint_tool.configure(config, found_files)
+        messages = pylint_tool.run(found_files)
+        for message in messages:
+            if message.code == "useless-suppression":
+                self.fail("useless-suppression was thrown")
