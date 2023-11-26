da0c3c6544f1ec6ee72e49162beed39168a8b551
Leonard <120810902+nemaean@users.noreply.github.com>
2023-11-26 17:13:38 +0000

    Replace legacy read_text with files api

--- prospector/blender.py
+++ prospector/blender.py
@@ -5,8 +5,8 @@
 # "Unused Import" warning on the same line. This is obviously redundant, so we
 # remove duplicates.
 from collections import defaultdict
-
 from importlib import resources
+
 import yaml
 
 __all__ = (
@@ -98,7 +98,7 @@ def blend(messages, blend_combos=None):
 
 
 def get_default_blend_combinations():
-    combos = yaml.safe_load(resources.read_text(__name__, "blender_combinations.yaml"))
+    combos = yaml.safe_load(resources.files(__package__).joinpath("blender_combinations.yaml").read_text())
     combos = combos.get("combinations", [])
 
     defaults = []
